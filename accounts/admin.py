d92a80b4b482b84f66e9cec1a2f454044d6e8dce
Sobhan Moradi <sobhan.mrds@gmail.com>
2025-07-24 11:59:30 -0400

    Finish Accounts App
    Write Views, Models, Urls, ..

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from accounts.models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Province)
+admin.site.register(City)
+admin.site.register(UserAddress)
